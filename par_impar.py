8202830694f1f9789e6d20d84c3c425b31381e31
joaompcabral <joaomateuspcabral@gmail.com>
2026-04-25 18:20:49 -0300

    Add par_impar.py to check if a number is even or odd

---
+++ par_impar.py
@@ -0,0 +1,10 @@
+import random
+
+numero = random.randint(1, 100)
+
+print(f"esse é o nosso numero: {numero}")
+
+if numero % 2 == 0:
+    print("esse numero é par")
+else :
+    print("esse numero é impar")
